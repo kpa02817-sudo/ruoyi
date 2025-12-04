6fe7ffef8aa7ba12c3c7513a86bdd0a4d393971e
pek <11@qq.com>
2025-12-04 18:16:22 +0800

    升级成mybatis-plus

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/PageUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/PageUtils.java
@@ -1,6 +1,6 @@
 package com.ruoyi.common.utils;
 
-import com.github.pagehelper.PageHelper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ruoyi.common.core.page.PageDomain;
 import com.ruoyi.common.core.page.TableSupport;
 import com.ruoyi.common.utils.sql.SqlUtil;
@@ -10,26 +10,33 @@ import com.ruoyi.common.utils.sql.SqlUtil;
  * 
  * @author ruoyi
  */
-public class PageUtils extends PageHelper
-{
+public class PageUtils {
     /**
      * 设置请求分页数据
      */
-    public static void startPage()
-    {
+    public static <T> Page<T> startPage() {
         PageDomain pageDomain = TableSupport.buildPageRequest();
         Integer pageNum = pageDomain.getPageNum();
         Integer pageSize = pageDomain.getPageSize();
-        String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
-        Boolean reasonable = pageDomain.getReasonable();
-        PageHelper.startPage(pageNum, pageSize, orderBy).setReasonable(reasonable);
-    }
+        // 创建 Page 对象
+        Page<T> page = new Page<>(pageNum, pageSize);
 
-    /**
-     * 清理分页的线程变量
-     */
-    public static void clearPage()
-    {
-        PageHelper.clearPage();
+        // 处理排序
+        String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
+        if (StringUtils.isNotEmpty(orderBy)) {
+            String[] orderBys = orderBy.split(",");
+            for (String order : orderBys) {
+                String[] parts = order.trim().split("\\s+");
+                if (parts.length == 2) {
+                    if ("asc".equalsIgnoreCase(parts[1])) {
+                        page.addOrder(com.baomidou.mybatisplus.core.metadata.OrderItem.asc(parts[0]));
+                    } else if ("desc".equalsIgnoreCase(parts[1])) {
+                        page.addOrder(com.baomidou.mybatisplus.core.metadata.OrderItem.desc(parts[0]));
+                    }
+                }
+            }
+        }
+
+        return page;
     }
 }
---
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/MybatisPlusConfig.java
@@ -0,0 +1,43 @@
+package com.ruoyi.framework.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
+/**
+ * Mybatis Plus 配置
+ * 
+ * @author ruoyi
+ */
+@Configuration
+@MapperScan("com.ruoyi.**.mapper")
+public class MybatisPlusConfig {
+    /**
+     * MyBatis-Plus 拦截器配置
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+
+        // 分页插件
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        // 设置请求的页面大于最大页后操作, true调回到首页，false 继续请求 默认false
+        paginationInnerInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        paginationInnerInterceptor.setMaxLimit(500L);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+
+        // 乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+
+        // 防止全表更新与删除插件
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+
+        return interceptor;
+    }
+}
